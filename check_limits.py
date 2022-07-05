134e8cc2789270736f48435141cd9a58e4939e8a
Shyamala Sarathy <105268170+shyamalasarathy@users.noreply.github.com>
2022-07-05 16:04:43 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -3,13 +3,12 @@ import soc_limits
 import charge_rate_limits
 
 def battery(temp,soc,charge):
-    temp_is_ok = temperature_limits.temp_is_ok(temp)
-    soc_is_ok = soc_limits.soc_is_ok(soc)
-    chargerate_is_ok = charge_rate_limits.charge_rate_is_ok(charge)
+    temp_ok = temperature_limits.temp_is_ok(temp)
+    soc_ok = soc_limits.soc_is_ok(soc)
+    chargerate_ok = charge_rate_limits.charge_rate_is_ok(charge)
     
-    if temp_is_ok and soc_is_ok and chargerate_is_ok:
+    if temp_ok and soc_ok and chargerate_ok:
         return True
-    
     return False
 
 if __name__ == '__main__':
